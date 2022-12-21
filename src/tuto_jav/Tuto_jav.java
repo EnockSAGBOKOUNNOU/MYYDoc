36a1576d6b4ad3ae6c5afc02309e4fd84019b987
Enock SAGBO KOUNNOU <enockkounou08@gmail.com>
2022-12-21 13:54:44 +0100

    mes tutorel

---
+++ src/tuto_jav/Tuto_jav.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tuto_jav;
+
+/**
+ *
+ * @author Enock SKY
+ */
+public class Tuto_jav {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
